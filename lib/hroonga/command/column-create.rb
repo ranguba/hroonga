0453e9d5b782a76e0919020de1f80a589dda0ae9
SHIMODA Hiroshi <piro@macian.clear-code.local>
2011-06-18 11:15:41 +0900

    implement column create command

---
+++ lib/hroonga/command/column-create.rb
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011  SHIMODA Hiroshi <shimoda@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+module Hroonga
+  module Command
+    class ColumnCreate
+      include Utils
+
+      def initialize(config)
+        @config = config
+      end
+
+      def call(env)
+        @env = env
+        process_request
+      end
+
+      private
+      def process_request
+        response = Rack::Response.new
+
+        create
+
+        response["Content-Type"] = "application/json"
+        response.write("{}")
+
+        response
+      end
+
+      def create
+        Groonga::Schema.define(:context => context) do |schema|
+          schema.change_table(request.table_name) do |table|
+            table.column(request.column_name, request.value_type, :type => request.column_type)
+          end
+        end
+      end
+    end
+  end
+end
